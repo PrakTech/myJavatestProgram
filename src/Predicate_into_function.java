6f687b83dd8a9328dc6fd68c9a260f648bf5ff92
prakash karkee <prakashkarkee92@gmail.com>
2020-02-09 14:07:54 +0545

    Reformat

--- src/Predicate_into_function.java
+++ src/Predicate_into_function.java
@@ -2,13 +2,13 @@ import java.util.function.Predicate;
 
 public class Predicate_into_function {
 
-     static void pred(int number, Predicate<Integer> predicate){
-         if (predicate.test(number)){
-             System.out.println("Given number is : " + number);
-         }
-     }
+    static void pred(int number, Predicate<Integer> predicate) {
+        if (predicate.test(number)) {
+            System.out.println("Given number is : " + number);
+        }
+    }
 
     public static void main(String[] args) {
-        pred(11, integer -> integer>10);
+        pred(11, integer -> integer > 10);
     }
 }
